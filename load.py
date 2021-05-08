0838135d58471692d1efb22aacf8db610374a8f7
XuweiyiChen <62165079+XuweiyiChen@users.noreply.github.com>
2021-05-08 10:58:28 +0800

    update for new methods

--- load.py
+++ load.py
@@ -1,78 +1,113 @@
-# encoding:utf-8
 from scipy.io import loadmat as load
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def reformat(samples, labels):
-    #  改变数据的形状
-    #（图片高，图片宽，通道数，图片数） -》 （图片数，图片高，图片宽，通道数）
-    #（   0,    1,    2,      3）  -》 （   3，   0,    1,      2）
-    new = np.transpose(samples,(3,0,1,2)).astype(np.float32)
+    # 改变原始数据的形状
+    #  0       1       2      3          3       0       1      2
+    # (图片高，图片宽，通道数，图片数) -> (图片数，图片高，图片宽，通道数)
+    new = np.transpose(samples, (3, 0, 1, 2)).astype(np.float32)
 
-    #labels 变成 one-hot encoding
-    labels = np.array([x[0] for x in labels])
+    # labels 变成 one-hot encoding, [2] -> [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    # digit 0 , represented as 10
+    # labels 变成 one-hot encoding, [10] -> [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    labels = np.array([x[0] for x in labels])  # slow code, whatever
     one_hot_labels = []
-
     for num in labels:
-        one_hot = [0.0]*10
-        if num ==10:
-            one_hot[0]=1.0
+        one_hot = [0.0] * 10
+        if num == 10:
+            one_hot[0] = 1.0
         else:
-            one_hot[num]=1.0
+            one_hot[num] = 1.0
         one_hot_labels.append(one_hot)
-    labels = np.array(one_hot_labels)
+    labels = np.array(one_hot_labels).astype(np.float32)
     return new, labels
 
 
 def normalize(samples):
-    # (R+G+B)/3
-    # 0~255 -> -1.0~1.0
-    a = np.add.reduce(samples, keepdims=True, axis=3)
-    a = a/3.0
-    return a/128.0-1.0
+    """
+    并且灰度化: 从三色通道 -> 单色通道     省内存 + 加快训练速度
+    (R + G + B) / 3
+    将图片从 0 ~ 255 线性映射到 -1.0 ~ +1.0
+    @samples: numpy array
+    """
+    a = np.add.reduce(samples, keepdims=True, axis=3)  # shape (图片数，图片高，图片宽，通道数)
+    a = a / 3.0
+    return a / 128.0 - 1.0
 
 
 def distribution(labels, name):
-    pass
+    # 查看一下每个label的分布，再画个统计图
+    # keys:
+    # 0
+    # 1
+    # 2
+    # ...
+    # 9
+    count = { }
+    for label in labels:
+        key = 0 if label[0] == 10 else label[0]
+        if key in count:
+            count[key] += 1
+        else:
+            count[key] = 1
+    x = []
+    y = []
+    for k, v in count.items():
+        # print(k, v)
+        x.append(k)
+        y.append(v)
 
-
-def inspect(dataset, labels, i):
-    print(labels[i])
-    plt.imshow(dataset[i].squeeze())
+    y_pos = np.arange(len(x))
+    plt.bar(y_pos, y, align='center', alpha=0.5)
+    plt.xticks(y_pos, x)
+    plt.ylabel('Count')
+    plt.title(name + ' Label Distribution')
     plt.show()
 
 
-train = load('data/train_32x32.mat')
-test = load('data/test_32x32.mat')
+def inspect(dataset, labels, i):
+    # 显示图片看看
+    if dataset.shape[3] == 1:
+        shape = dataset.shape
+        dataset = dataset.reshape(shape[0], shape[1], shape[2])
+    print(labels[i])
+    plt.imshow(dataset[i])
+    plt.show()
+
+
+train = load('../../2-3/data/train_32x32.mat')
+test = load('../../2-3/data/test_32x32.mat')
+
+
+# print('Train Samples Shape:', train['X'].shape)
+# print('Train  Labels Shape:', train['y'].shape)
+
+# print('Train Samples Shape:', test['X'].shape)
+# print('Train  Labels Shape:', test['y'].shape)
+
 
 train_samples = train['X']
 train_labels = train['y']
-
-# print(type(train_samples))
-# print(type(train_labels))
-
 test_samples = test['X']
 test_labels = test['y']
 
-n_train_samples, _train_labels = reformat(train_samples,train_labels)
-print(n_train_samples.shape)
-print(_train_labels.shape)
+
+n_train_samples, _train_labels = reformat(train_samples, train_labels)
 n_test_samples, _test_labels = reformat(test_samples, test_labels)
 
 _train_samples = normalize(n_train_samples)
 _test_samples = normalize(n_test_samples)
 
-
-
 num_labels = 10
 image_size = 32
 num_channels = 1
 
-
-
 if __name__ == '__main__':
-    _train_samples = normalize(_train_samples)
-    print(_train_samples.shape)
-    inspect(_train_samples, _train_labels, 19999)
-    # distribution(train_labels, 'Train Labels')
-
+    # 探索数据
+    inspect(_train_samples, _train_labels, 1234)
+# _train_samples = normalize(_train_samples)
+# inspect(_train_samples, _train_labels, 1234)
+# distribution(train_labels, 'Train Labels')
+# distribution(test_labels, 'Test Labels')
